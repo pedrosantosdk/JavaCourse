30bf011a875a4b485a9b520349c0686e3a85e785
Your Name <pedrodevpro@gmail.com>
2022-06-07 21:07:02 -0300

    Removing files and adding exercises.

--- src/com/javacourse/lesson15/GasStation.java
+++ src/com/javacourse/lesson15/GasStation.java
@@ -18,35 +18,36 @@ public class GasStation {
         if (choice.equalsIgnoreCase("Ethanol")) {
             if (liters <= 20) {
                 double fill = ethanol * liters;
-                int discount = 3 * liters;
+                int discount = 3;
                 double total = (fill / 100) * discount;
                 double paid = fill - total;
+                System.out.println("\nE-Ethanol");
                 System.out.printf("\nYou have filled %d liters of ethanol.", liters);
                 System.out.printf("\nYou paid: $%.2f ", paid);
             } else if (liters > 20) {
                 double fill = ethanol * liters;
-                int discount = 5 * liters;
+                int discount = 5;
                 double total = (fill / 100) * discount;
                 double paid = fill - total;
-                System.out.println("E-Ethanol");
+                System.out.println("\nE-Ethanol");
                 System.out.printf("\nYou have filled %d liters of ethanol.", liters);
                 System.out.printf("\nYou paid: $%.2f ", paid);
             }
         } else if (choice.equalsIgnoreCase("Gasoline")) {
             if (liters <= 20) {
                 double fill = gasoline * liters;
-                int discount = 4 * liters;
+                int discount = 4;
                 double total = (fill / 100) * discount;
                 double paid = fill - total;
-                System.out.println("G-Gasoline");
+                System.out.println("\nG-Gasoline");
                 System.out.printf("\nYou have filled %d liters of gasoline.", liters);
                 System.out.printf("\nYou paid: $%.2f ", paid);
             } else if (liters > 20) {
                 double fill = gasoline * liters;
-                int discount = 6 * liters;
+                int discount = 6;
                 double total = (fill / 100) * discount;
                 double paid = fill - total;
-                System.out.println("G-Gasoline");
+                System.out.println("\nG-Gasoline");
                 System.out.printf("\nYou have filled %d liters of gasoline.", liters);
                 System.out.printf("\nTotal: $%.2f ", paid);
             }

ca13afe17883041605383c6c0357b28d01a087e8
pedrosantosdk <pedrodevpro@gmail.com>
2022-06-13 17:14:19 -0300

    Adding exercise.

---
+++ src/com/javacourse/lesson17/Count.java
@@ -0,0 +1,16 @@
+package com.javacourse.lesson17;
+
+public class Count {
+    public static void main(String[] args) {
+
+        // Count from 1 to 20 with the numbers one under the other.
+        /*for(int i = 1; i <= 20; i++){
+            System.out.println(i);
+        }*/
+
+        // Counts from 1 to 20 with the numbers in sequence on the same line.
+        for(int i = 1; i <= 20; i++){
+            System.out.print(i);
+        }
+    }
+}

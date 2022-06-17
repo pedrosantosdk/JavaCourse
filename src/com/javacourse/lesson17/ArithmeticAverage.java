dadbf436484d9610d861a4e98f9d2d50c19e6117
pedrosantosdk <pedrodevpro@gmail.com>
2022-06-17 15:12:17 -0300

    Adding exercises.

--- src/com/javacourse/lesson17/ArithmeticAverage.java
+++ src/com/javacourse/lesson17/ArithmeticAverage.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 public class ArithmeticAverage {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-<<<<<<< HEAD
-=======
         double count = 0;
->>>>>>> origin/master
         double number = 0;
 
         System.out.println("Enter the quantity of grades: ");
@@ -16,18 +13,11 @@ public class ArithmeticAverage {
 
         for(int i = 0; i < grades; i++){
             System.out.println("Enter a number: ");
-<<<<<<< HEAD
             number += input.nextDouble();
 
         }
         double result = number / grades;
         System.out.printf("The average of the grades was: %.2f",result);
-=======
-            number += input.nextInt();
-            count ++;
         }
-        double result = number / grades;
-        System.out.println(result);
->>>>>>> origin/master
     }
-}
+
